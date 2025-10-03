ef2cd8cbf64ba9b976c99f55b710bf0f77934373
denmaster <denmaster.den.lan@gmail.com>
2025-10-04 01:31:22 +1000

    2025-10-04 01:31:20 T003 completed: created parser directory structure tabbyAPI/common/parsers/

---
+++ common/parsers/__init__.py
@@ -0,0 +1,9 @@
+"""
+GLM-4 MoE Parser Integration for TabbyAPI.
+
+This module provides tool call and reasoning parsers for GLM-4.5 family models.
+ExLlamaV3 backend only.
+"""
+
+# Exports will be added as parsers are implemented
+__all__ = []
